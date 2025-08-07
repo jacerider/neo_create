6fd03236b88b7cb3fdfab7446f60f594fec7dcff
JaceRider <jacerider@gmail.com>
2025-08-07 16:23:31 -0500

    Add GrumPHP to the mix.

--- src/Commands/DrushCommands.php
+++ src/Commands/DrushCommands.php
@@ -126,7 +126,8 @@ class DrushCommands extends CoreCommands {
     $commands = [];
     $commands['Setting minimum-stability to dev.'] = 'composer config minimum-stability dev';
     $commands['Installing modules and themes.'] = 'composer require ' . implode(' ', $composerRequire);
-    $commands['Configuring VScode for Drupal. '] = 'composer config --json --merge extra.installer-paths \'{".vscode/extensions/{$name}": ["type:vscode-extension"]}\' && composer config --json --merge extra.installer-types \'["vscode-extension"]\' && composer config scripts.vscode-setup "VscodeDrupal\\Install::postPackageInstall" && composer require jacerider/vscode-neo && composer vscode-setup -- --color=' . $color;
+    $commands['Configuring VScode for Drupal. '] = 'composer config --json --merge extra.installer-paths \'{".vscode/extensions/{$name}": ["type:vscode-extension"]}\' && composer config --json --merge extra.installer-types \'["vscode-extension"]\' && composer config scripts.vscode-setup "VscodeDrupal\\Install::postPackageInstall" && composer require --dev jacerider/vscode-neo && composer vscode-setup -- --color=' . $color;
+    $commands['Installing GrumpPHP.'] = 'composer require --dev jacerider/grumphp-drupal';
     foreach ($commands as $message => $command) {
       $this->io->info($message);
       $shell = Drush::shell($commandPrefix . $command, $this->getRoot());
